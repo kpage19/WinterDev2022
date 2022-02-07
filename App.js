c18f4f688ba2c18e2eb411e945e97e9df9a0addd
kpage19 <78561467+kpage19@users.noreply.github.com>
2022-02-07 12:38:02 -0800

    react file and css stylesheet

---
+++ App.js
@@ -0,0 +1,13 @@
+import '../src/App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>Study Buddies</h1>
+      <h2>login</h2>
+      <p id="center">We need some kind of tagline here like
+          Where students can find study buddies</p>
+    </div>
+  );
+}
+
+export default App;
