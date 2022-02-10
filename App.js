74079407bbe10ff65cd5a9051214fd1a45baeae8
kpage19 <78561467+kpage19@users.noreply.github.com>
2022-02-09 18:15:09 -0800

    Add files via upload
    Added buttons and formatting

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import '../src/App.css';
+
+
 function App() {
+  
   return (
+
     <div className="App">
+      <iframe id="topbar"></iframe>
       <h1>Study Buddies</h1>
-      <h2>login</h2>
+      <button id="login">Login</button>
+      <button id="signupSmall">Sign up</button>
       <p id="center">We need some kind of tagline here like
           Where students can find study buddies</p>
+      <button id="signupBig">Sign up</button>
     </div>
   );
 }
 
 export default App;
+
