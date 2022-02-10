ff800639d53b165d800e6ef55366f164ac0cb802
kpage19 <78561467+kpage19@users.noreply.github.com>
2022-02-09 21:27:20 -0800

    reorganized, functionally the same

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import '../src/App.css';
 
-
 function App() {
   
   return (
 
     <div className="App">
       <iframe id="topbar"></iframe>
-      <h1>Study Buddies</h1>
+      <h1 id="StudyBuddies">Study Buddies</h1>
       <button id="login">Login</button>
       <button id="signupSmall">Sign up</button>
       <p id="center">We need some kind of tagline here like
@@ -16,6 +15,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
-
